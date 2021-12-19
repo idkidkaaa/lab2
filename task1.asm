caa8bc0ad7134e7afb26a9026f40f2b92703106f
idkidkaaa <95030943+idkidkaaa@users.noreply.github.com>
2021-12-19 14:44:19 +0300

    Add files via upload

---
+++ task1.asm
@@ -0,0 +1,32 @@
+%include "io.inc"
+
+section .text
+global CMAIN
+CMAIN:
+    mov ebp, esp ; for correct debugging
+    xor eax, eax  
+    
+    lea      ebx, [array]
+    mov      ecx, 8
+    
+    push     ebx
+    push     ecx
+    
+next:    
+    mov      dx,  [ebx]
+    cmp      dx,  0
+    jl       skip
+    add      ax,  dx
+
+skip:            
+    add      ebx, 4  
+    loop     next   
+    pop      ecx
+    pop      ebx
+    
+    PRINT_DEC 1, ax
+    
+    ret
+    
+section .data
+array: DD 6, -4, 11, -2, -9, 4, -5, 0
